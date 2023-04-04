b1df38d786b8f1b4b8d9b8df6a5b9e67c786efe3
KunalHS <kunal.hotwani12345@gmail.com>
2023-04-04 17:58:00 +0530

    Login authentication added

---
+++ src/db/dbConnection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+module.exports = async function connectToDB() {
+	mongoose.set("strictQuery", true);
+	try {
+		await mongoose.connect(
+			process.env.MONGODB_CONNECTION_STRING,
+			{ useNewUrlParser: true },
+			{ useUnifiedTopology: true }
+		);
+		console.log("MongoDB is connected");
+	} catch (error) {
+		console.error("Error connecting to MongoDB:", error);
+	}
+}
