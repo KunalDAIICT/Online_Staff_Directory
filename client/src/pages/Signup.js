545ffe3799894981c183088c31d5189b2071e2da
Devanshi <devanshipujara02@gmail.com>
2023-04-07 00:47:33 +0530

    Connected signup to backend

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -17,19 +17,86 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 //import MuiPhoneNumber from "material-ui-phone-number";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
 
 
 const theme = createTheme();
 
+
+
+
+
+
+// export const Signup = () => {
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     console.log({
+//       email: data.get('email'),
+//       password: data.get('password'),
+//     });
+//   };
+
 export const Signup = () => {
+  let navigate=useNavigate();
+  const handleUserSignup = async (userData) => {
+    
+    const response = await fetch("http://localhost:3000/signUp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    console.log(response);
+  
+    if(response.status === 200){
+      alert("Registered successfully, You can now login");
+      navigate('/loginpage');
+    }
+    else if(response.status === 400){
+      alert("Email address already in use! Try again!");
+    }
+    else{
+      alert("An unknown error occured please try again!");
+    }
+  };
+
   const handleSubmit = (event) => {
+
+
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+
+    var a="0";
+    if(data.get('role') === "faculty"){
+      a="1";
+    }
+    console.log(a);
+    // const userData = 
+    //   {
+    //     "confirmpassword": "p",
+    //     "mobile_number": "07623065210",
+    //     "name": "Kunal Hotwani",
+    //     "password": "p",
+    //     "role": "0",
+    //     "university": "DAIICT",
+    //     "userEmail": "kunal.hotwani1@gmail.com"
+    //   };
+    
+    const userData = {
+      "name" : data.get('firstName') + " " + data.get('lastName'),
+      "userEmail": data.get('email'),
+      "password": data.get('password'),
+      "confirmpassword" : data.get('confirm password'),
+      "mobile_number": data.get('mobile'),
+      "university": data.get('university'),
+      "role": a
+    };
+    console.log(userData);
+    handleUserSignup(userData);
   };
+  
 
   return (
     <ThemeProvider theme={theme}>
