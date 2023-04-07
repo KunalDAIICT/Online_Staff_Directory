d991b0ab706a2980d60d0e50772314180a68e778
KunalDAIICT <>
2023-04-07 10:02:20 +0530

    Profile API changes

--- src/controllers/serverController.js
+++ src/controllers/serverController.js
@@ -18,7 +18,7 @@ module.exports = async function startServer() {
 
 	app.post("/signUp", signUpController);
 	app.post("/login", loginController);
-	app.post("/studentProfile", reqHandler.getStudentProfile);
+	app.post("/profile", reqHandler.getProfile);
 	
 	app.listen(process.env.PORT_NUM, function (req, res) {
 		console.log("Server setup complete, Listening on Port 3000");
