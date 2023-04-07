bd120eb1bc89802cb8c5b829ee146ea41ee78b5a
KunalDAIICT <202001468@daiict.ac.in>
2023-04-07 12:06:48 +0530

    Fixed Login Bug

--- src/controllers/requestHandler.js
+++ src/controllers/requestHandler.js
@@ -4,22 +4,22 @@ const facultyDetailsCollection = require("../models/facultyDetailsModel");
 
 function getProfile(req, res) {
 	const token = req.headers.authorization.split(" ")[1];
-	const student = authorize(token, process.env.ACESS_TOKEN_SECRET);
-	if (!student) {
+	const user = authorize(token, process.env.ACESS_TOKEN_SECRET);
+	if (!user) {
 		return res.status(401).json({ error: "Invalid token" });
 	}
-	studentCollection.findOne(student, function (err, student) {
+	studentCollection.findOne(user, function (err, user) {
 		if (err) {
 			console.error(err);
 			return res.status(500).json({ error: "Internal server error" });
 		}
 
-		if (!student) {
+		if (!user) {
 			return res.status(404).json({ error: "User not found" });
 		}
 
-		// Send the student profile in the response
-		res.status(200).json(student);
+		// Send the user profile in the response
+		res.status(200).json(user);
 	});
 }
 
