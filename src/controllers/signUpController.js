bef8033871eb323c5e2288441764b77e716e97f8
vansh-parikh <124344810+vansh-parikh@users.noreply.github.com>
2023-04-24 00:13:16 +0530

    Add files via upload

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,82 +1,82 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
 const studentCollection = require("../models/studentModel");
 const facultyCollection = require("../models/facultyModel");
 const facultyDetailsCollection = require("../models/facultyDetailsModel");
+const sendAuthMail = require("./emailVerification"); // import the email verification function
 
 module.exports = function (req, res) {
-	let userInstance;
-	if (req.body.role == 0) {
-		//student
-		studentCollection.exists({ _id: req.body.userEmail }, function (err, doc) {
-			if (doc) {
-				console.log("User already exist");
-				res.status(400).json({
-					message: "This e-mail address is already in use!",
-				});
-			} else {
-				userInstance = new studentCollection({
-					_id: req.body.userEmail,
-					name: req.body.name,
-					password: req.body.password,
-					mobile_number: req.body.mobile_number,
-					university: req.body.university,
-					role: req.body.role,
-					verified: false,
-					Image: req.body.Image,
-				});
-				userInstance.save();
-				console.log("New Student added");
-				res.status(200).json({ message: "You are registered successfully" });
-			}
-		});
-	} else {
-		facultyCollection.exists({ _id: req.body.userEmail }, function (err, doc) {
-			if (doc) {
-				console.log("User already exist");
-				res.status(400).json({
-					message: "This e-mail address is already in use!",
-				});
-			} else {
-				userInstance = new facultyCollection({
-					_id: req.body.userEmail,
-					name: req.body.name,
-					password: req.body.password,
-					mobile_number: req.body.mobile_number,
-					university: req.body.university,
-					role: req.body.role,
-					specialization: req.body.specialization,
-					experience: req.body.experience,
-					projects: req.body.projects,
-					Awards_and_Honors: req.body.Awards_and_Honors,
-					Industrial_experience: req.body.Industrial_experience,
-					Publications: req.body.Publications,
-					verified: false,
-					Image: req.body.Image,
-				});
-				userInstance.save();
-				facultyDetailsInstance = new facultyDetailsCollection({
-					_id: req.body.userEmail,
-					name: req.body.name,
-					mobile_number: req.body.mobile_number,
-					university: req.body.university,
-					specialization: req.body.specialization,
-					experience: req.body.experience,
-					projects: req.body.projects,
-					Awards_and_Honors: req.body.Awards_and_Honors,
-					Industrial_experience: req.body.Industrial_experience,
-					Publications: req.body.Publications,
-					isApproved: false,
-					Image: req.body.Image,
-				});
-				facultyDetailsInstance.save();
-				console.log("New Faculty added");
-				res.status(200).json({ message: "You are registered successfully" });
-			}
-		});
-	}
-	const token = jwt.sign(
-		{ _id: req.body.userEmail },
-		process.env.ACESS_TOKEN_SECRET
-	);
+    if (req.body.role == 0) {
+        //student
+        studentCollection.exists({ _id: req.body.userEmail }, function (err, doc) {
+            if (doc) {
+                console.log("User already exist");
+                res.statusMessage =
+                    'This e-mail address is already in use, <a href="./login.html">login</a>';
+                res.sendStatus(400);
+            } else {
+                studentInstance = new studentCollection({
+                    _id: req.body.userEmail,
+                    name: req.body.name,
+                    password: req.body.password,
+                    mobile_number: req.body.mobile_number,
+                    university: req.body.university,
+                    role: req.body.role,
+					isVerified: false,
+                });
+                studentInstance.save();
+                console.log("New Student added");
+                sendAuthMail(req.body.userEmail,req.body.name,req.body.role);
+                res.statusMessage =
+                    'You are registered, head to the <a href="./login.html">login</a> page';
+                res.sendStatus(200);
+            }
+        });
+    } else {
+        facultyCollection.exists({ _id: req.body.userEmail }, function (err, doc) {
+            if (doc) {
+                console.log("User already exist");
+                res.statusMessage =
+                    'This e-mail address is already in use, <a href="./login.html">login</a>';
+                res.sendStatus(400);
+            } else {
+                facultyInstance = new facultyCollection({
+                    _id: req.body.userEmail,
+                    name: req.body.name,
+                    password: req.body.password,
+                    mobile_number: req.body.mobile_number,
+                    university: req.body.university,
+                    role: req.body.role,
+                    specialization: req.body.specialization,
+                    experience: req.body.experience,
+                    projects: req.body.projects,
+                    Awards_and_Honors: req.body.Awards_and_Honors,
+                    Industrial_experience: req.body.Industrial_experience,
+                    Publications: req.body.Publications,
+                });
+                facultyInstance.save();
+                facultyDetailsInstance = new facultyDetailsCollection({
+                    _id: req.body.userEmail,
+                    name: req.body.name,
+                    mobile_number: req.body.mobile_number,
+                    university: req.body.university,
+                    specialization: req.body.specialization,
+                    experience: req.body.experience,
+                    projects: req.body.projects,
+                    Awards_and_Honors: req.body.Awards_and_Honors,
+                    Industrial_experience: req.body.Industrial_experience,
+                    Publications: req.body.Publications,
+                    isApproved: false,
+					isVerified: false,
+                });
+                facultyDetailsInstance.save();
+                console.log("New Faculty added");
+
+                sendAuthMail(req.body.userEmail,req.body.name,req.body.role);
+
+                res.statusMessage =
+                    'You are registered, head to the <a href="./login.html">login</a> page';
+                res.sendStatus(200);
+            }
+        });
+    }
 };
+
