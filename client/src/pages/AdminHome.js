ba1e15bd28930c54e57c73c475be895a18220ebf
Jay Kuvadiya <jaykuvadiya@Jays-MacBook-Pro.local>
2023-04-24 19:57:08 +0530

    AdminHone Bug fixes

--- client/src/pages/AdminHome.js
+++ client/src/pages/AdminHome.js
@@ -1,173 +1,178 @@
-import React, { useState } from 'react';
-import '../admin_home.css'; // import CSS styles
-import { AppBar, Toolbar, Tabs, Tab, Stack, Typography, Button, IconButton, Box, Container, InputAdornment, TextField, Drawer } from '@mui/material';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import LinkIcon from '@mui/icons-material/Link';
-import SchoolIcon from '@mui/icons-material/School';
-import SearchIcon from '@mui/icons-material/Search';
-import DoneIcon from '@mui/icons-material/Done';
-import ClearIcon from '@mui/icons-material/Clear';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-
+import React, { useState } from "react";
+import "../admin_home.css"; // import CSS styles
+import {
+  AppBar,
+  Toolbar,
+  Tabs,
+  Tab,
+  Stack,
+  Typography,
+  Button,
+  IconButton,
+  Box,
+  Container,
+  InputAdornment,
+  TextField,
+  Drawer,
+} from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import LinkIcon from "@mui/icons-material/Link";
+import SchoolIcon from "@mui/icons-material/School";
+import SearchIcon from "@mui/icons-material/Search";
+import DoneIcon from "@mui/icons-material/Done";
+import ClearIcon from "@mui/icons-material/Clear";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { useNavigate } from "react-router-dom";
 
 // This page will show the data of the approved faculty members to the admin. The admin can delete the faculty members from this page.
 
 // faculty data
-const facultyData = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        mobile: '+1 123-456-7890',
-        specialization: 'Computer Science',
-        isapproved: true,
-        imageUrl: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-    },
-    {
-        id: 2,
-        name: 'Saurabh Tiwari',
-        email: 'janesmith@example.com',
-        mobile: '+1 234-567-8901',
-        specialization: "Mathematics",
-        isapproved: false,
-        imageUrl: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-    },
-    {
-        id: 3,
-        name: 'Anil Roy',
-        email: 'janesmith@example.com',
-        mobile: '+1 234-567-8901',
-        specialization: 'Mathematics',
-        isapproved: false,
-        imageUrl: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-    },
-    {
-        id: 4,
-        name: 'Jane Smith',
-        email: 'janesmith@example.com',
-        mobile: '+1 234-567-8901',
-        specialization: 'Mathematics',
-        isapproved: true,
-        imageUrl: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-    },
-    {
-        id: 5,
-        name: 'Jane Smith',
-        email: 'janesmith@example.com',
-        mobile: '+1 234-567-8901',
-        specialization: 'Mathematics',
-        isapproved: true,
-        imageUrl: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-    },
-];
+
+
+
 
 
 const FacultyCardApproved = ({ faculty }) => (
 
-    <Box className="faculty-card">
-        <div className="faculty-image-container">
-            <img className="faculty-image" src={faculty.imageUrl} alt={faculty.name} />
-        </div>
-        <div className="faculty-details">
-            <h3 className="faculty-name">{faculty.name}</h3>
-            <p className="faculty-email">{faculty.email}</p>
-            <p className="faculty-mobile">{faculty.mobile}</p>
-            <p className="faculty-specialization">{faculty.specialization}</p>
-            <p>
-                <IconButton>
-                    <LinkedInIcon />
-                </IconButton>
-                <IconButton>
-                    <SchoolIcon />
-                </IconButton>
-                <IconButton>
-                    <LinkIcon />
-                </IconButton>
-            </p>
-            <p><Button variant="contained" color="error">Delete</Button></p>
-        </div>
-    </Box>
+  <Box className="faculty-card">
+    <div className="faculty-image-container">
+      <img className="faculty-image" src={faculty.Image} alt={faculty.name} />
+    </div>
+    <div className="faculty-details">
+      <h3 className="faculty-name">{faculty.name}</h3>
+      <p className="faculty-email">{faculty._id}</p>
+      <p className="faculty-mobile">{faculty.mobile_number}</p>
+      <p className="faculty-specialization">{faculty.specialization}</p>
+      <p>
+        <IconButton>
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton>
+          <SchoolIcon />
+        </IconButton>
+        <IconButton>
+          <LinkIcon />
+        </IconButton>
+      </p>
+      <p>
+        <Button variant="contained" color="error">
+          Delete
+        </Button>
+      </p>
+    </div>
+  </Box>
 );
 
 
 // faculty card component
 const FacultyCardNotApproved = ({ faculty }) => (
 
-    <Box className="faculty-card">
-        <div className="faculty-image-container">
-            <img className="faculty-image" src={faculty.imageUrl} alt={faculty.name} />
-        </div>
-        <div className="faculty-details">
-            <h3 className="faculty-name">{faculty.name}
-            </h3>
-            <p className="faculty-email">{faculty.email}</p>
-            <p className="faculty-mobile">{faculty.mobile}</p>
-            <p className="faculty-specialization">{faculty.specialization}</p>
-            <p>
-                <IconButton>
-                    <LinkedInIcon />
-                </IconButton>
-                <IconButton>
-                    <SchoolIcon />
-                </IconButton>
-                <IconButton>
-                    <LinkIcon />
-                </IconButton>
-            </p>
-            <Stack direction="row" spacing={1.5}>
-                <Button variant="contained" color="success" startIcon={<DoneIcon />}>Approve</Button>
-                <Button variant="contained" color="error" startIcon={<ClearIcon />}>Delete</Button>
-            </Stack>
-        </div>
-    </Box>
+  <Box className="faculty-card">
+    <div className="faculty-image-container">
+      <img className="faculty-image" src={faculty.Image} alt={faculty.name} />
+    </div>
+    <div className="faculty-details">
+      <h3 className="faculty-name">{faculty.name}</h3>
+      <p className="faculty-email">{faculty._id}</p>
+      <p className="faculty-mobile">{faculty.mobile_number}</p>
+      <p className="faculty-specialization">{faculty.specialization}</p>
+      <p>
+        <IconButton>
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton>
+          <SchoolIcon />
+        </IconButton>
+        <IconButton>
+          <LinkIcon />
+        </IconButton>
+      </p>
+      <Stack direction="row" spacing={1}>
+        <Button variant="contained" color="success" startIcon={<DoneIcon />}>
+          Approve
+        </Button>
+        <Button variant="contained" color="error" startIcon={<ClearIcon />}>
+          Delete
+        </Button>
+      </Stack>
+    </div>
+  </Box>
 );
 
 
 // faculty details page component
-const FacultyDetails = (isapproved) => {
-    // const checkapproved = isapproved;
-    return (
-        <div>
-            <div className="faculty-details-page">
-                <div className="faculty-cards-box">
-                    <div className="faculty-cards-container">
-                        {isapproved ? (
-                            facultyData.filter(faculty => {
-                                return(
-                                    faculty.isapproved === true
-                                );
-                            }).map((faculty) => (
-                                <FacultyCardApproved key={faculty.id} faculty={faculty} />
-                            ))
-                        ) : (
-                            facultyData.filter(faculty => {
-                                return(
-                                    faculty.isapproved === false
-                                );
-                            }).map((faculty) => (
-                                <FacultyCardNotApproved key={faculty.id} faculty={faculty} />
-                            ))
-                        )}
-                    </div>
-                </div>
-            </div>
+const FacultyDetails = (isapproved, allfaculties) => {
+  // const checkapproved = isapproved;
+  return (
+    <div>
+      <div className="faculty-details-page">
+        <div className="faculty-cards-box">
+          <div className="faculty-cards-container">
+            {isapproved
+              ? allfaculties
+                  .filter((faculty) => {
+                    return faculty.isApproved === true;
+                  })
+                  .map((faculty) => (
+                    <FacultyCardApproved key={faculty.id} faculty={faculty} />
+                  ))
+              : allfaculties
+                  .filter((faculty) => {
+                    return faculty.isApproved === false;
+                  })
+                  .map((faculty) => (
+                    <FacultyCardNotApproved
+                      key={faculty.id}
+                      faculty={faculty}
+                    />
+                  ))}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default function LabTabs() {
-    const [value, setValue] = React.useState('1');
+  const [value, setValue] = React.useState("1");
+  const [allfaculties, setAllfaculties] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/admin/allfaculties", {
+        method: "GET",
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      console.log(response);
+      const json = await response.json();
+      setAllfaculties(json);
+      console.log(json);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  fetchData();
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  var token=localStorage.getItem("token");
+  let navigate=useNavigate();
+  
+  const logOut = () => {
+    localStorage.setItem("token","null");
+    token=null;
+    alert("Logged out successfully");
+    navigate("/");
+  }
 
-    return (
-        <Box sx={{ width: '100%', typography: 'body1' }}>
-
-            <AppBar position="static" className='Navbar'>
+  return (
+    <Box sx={{ width: "100%", typography: "body1" }}>
+      <Box padding={1} border sx={{ flexGrow: 1 }}>
+        <AppBar position="static" className="Navbar">
           <Toolbar>
             <IconButton
               size="large"
@@ -175,39 +180,29 @@ export default function LabTabs() {
               color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
-            >
-              {/* <MenuIcon /> */}
-            </IconButton>
+            ></IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Online Faculty-Staff Directory
+              <b>Online Faculty-Staff Directory</b>
             </Typography>
-            <Button color="inherit">Profile</Button>
-            <Button color="inherit" variant='outlined' sx={{ mr: 1 }}>LogOut</Button>
+            <Button color="warning" variant="contained" onClick={logOut}>
+              Log out
+            </Button>
           </Toolbar>
         </AppBar>
+      </Box>
 
-            <TabContext value={value}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabList onChange={handleChange} aria-label="lab API tabs example">
-                        <Tab label="Approved Faculties" value="1" />
-                        <Tab label="Pending Approvals" value="2" />
-                        <Tab label="Universities" value="3" />
-                    </TabList>
-                </Box>
-                <TabPanel value="1">
-                    {FacultyDetails(true)}
-                </TabPanel>
-                <TabPanel value="2">
-                    {FacultyDetails(false)}
-                </TabPanel>
-                <TabPanel value="3">
-                    
-                </TabPanel>
-            </TabContext>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Approved Faculties" value="1" />
+            <Tab label="Pending Approvals" value="2" />
+            <Tab label="Universities" value="3" />
+          </TabList>
         </Box>
-    );
+        <TabPanel value="1">{FacultyDetails(true, allfaculties)}</TabPanel>
+        <TabPanel value="2">{FacultyDetails(false, allfaculties)}</TabPanel>
+        <TabPanel value="3"></TabPanel>
+      </TabContext>
+    </Box>
+  );
 }
-
-
-
-
