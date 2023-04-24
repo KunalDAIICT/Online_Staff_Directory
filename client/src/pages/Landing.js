82b2833c2ca5a95a05c1e806bbe1c34e1ad48fe1
Devansh Patel <75575802+dp2292@users.noreply.github.com>
2023-04-24 12:54:59 +0530

    added dynamic university adding features

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,47 +1,14 @@
-import { Link } from "react-router-dom"
 import React, { useState } from 'react';
-import '../landing.css';  // path for css file
+import '../university.css'; // import CSS styles
 import { AppBar, Toolbar, Stack, Typography, Fab, Button, IconButton, Box, Container, InputAdornment, TextField, Divider } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-// aa logos
-import logo from '../data/Logo.png'
-import nirma_logo from '../data/nirma_logo.png'
-import iitb_logo from '../data/iitb_logo.png'
-import au_logo from '../data/au_logo.png'
-import pdeu_logo from '../data/pdeu_logo.png'
+ // array of all universities
 
-const universityData = [
-    {
-        id: 1,
-        university_name: 'Dhirubhai Ambani Institute of Information and Communication Technology',
-        imageUrl: logo,
-    },
-    {
-        id: 2,
-        university_name: 'Nirma University',
-        imageUrl: nirma_logo,
-    },
-    {
-        id: 3,
-        university_name: 'Indian Institute of Technology Bombay',
-        imageUrl: iitb_logo,
-    },
-    {
-        id: 4,
-        university_name: 'Ahmedabad University',
-        imageUrl: au_logo,
-    },
-    {
-        id: 5,
-        university_name: 'Pandit Deendayal Energy University',
-        imageUrl: pdeu_logo,
-    },
-];
+
 
 export function SearchBar() {
     const [searchTerm, setSearchTerm] = useState("");
-  
     const handleChange = (event) => {
       setSearchTerm(event.target.value);
     };
@@ -70,14 +37,14 @@ export function SearchBar() {
 
 const UniversityCard = ({ university }) => (
     <>
-        <Stack className="university-card" direction="row" marginLeft={30} marginTop={10} alignItems={'center'}>
+        <Stack className="university-card" direction="row" marginLeft={40} marginTop={10} alignItems={'center'}>
             <Box display='flex' height='100%' width='25%' alignItems='center' justifyContent='center' justifyItems='center'>
-                <img className="university-image" src={university.imageUrl} alt={'logo'} />
+                <img className="university-image" src={university.Image} alt={'logo'} />
             </Box>
             <Divider orientation="vertical" />
             <Box display='flex' width="65%" alignItems='center' justifyContent='center' marginRight='5%' marginLeft='%' overflow='hidden'>
                 <Typography fontSize={20} textAlign='center'>
-                    {university.university_name}
+                    {university.name}
                 </Typography>
             </Box>
             <Box display='flex' height='100%' width="10%" alignItems='center' justifyContent='center'>
@@ -89,38 +56,24 @@ const UniversityCard = ({ university }) => (
     </>
 )
 
+export const UniversityDetails = () => {
 
-
-// export const Landing = () => {
-
-//     const logout = () => {
-//         localStorage.setItem("token","null");
-//     }
-//     return(
-//         <div className="App">
-//             This is the landing page
-//             <br />
-//             <Link to={'/loginpage'}><button>Login</button></Link>
-//             OR 
-//             <Link to={'/signup'}><button>Sign Up</button></Link>
-
-//             <br />
-//             <Link to={'/university'}><button>Go to University</button></Link>
-
-
-//             <br />
-
-//             {/* {localStorage.getItem("token")}; */}
-
-//             <button onClick={logout}>Logout</button>
-//         </div>
-//     )
-// }
-
-export const Landing = () => {
-    const logout = () => {
-        localStorage.setItem("token","null");
-    }
+    const [allUni, setAllUni] = useState([]);
+    const fetchData = async () => {
+        try {
+            const response = await fetch("http://localhost:3000/getUniversities", {
+                method: "GET",
+            });
+            console.log(response);
+            const json = await response.json();
+            setAllUni(json);
+            console.log(json);
+        }
+        catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+    fetchData();
     
     return (
         <>
@@ -143,9 +96,8 @@ export const Landing = () => {
                         <Button color="inherit">Academics</Button>
                         <Button color="inherit">Profile</Button>
                         <Button color="inherit">About Us</Button>
-                        <Link to={'/loginpage'}><Button color="inherit" variant='outlined' sx={{ mr: 1 }}>Sign In</Button></Link>
-                        <Link to={'/signup'}><Button color="warning" variant='contained'>Sign Up</Button></Link>
-                        
+                        <Button color="inherit" variant='outlined' sx={{ mr: 1 }}>Sign In</Button>
+                        <Button color="warning" variant='contained'>Sign Up</Button>
                     </Toolbar>
                 </AppBar>
             </Box>
@@ -159,11 +111,11 @@ export const Landing = () => {
                     </Fab>
                 </item> */}
             </Stack>
-            {universityData.map(university => (
+            {allUni.map(university => (
                 <UniversityCard key={university.id} university={university} />
             ))}
         </>
     );
 };
 
-export default Landing;
+export default UniversityDetails;
