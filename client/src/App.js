db725dfcfd0d4aa0a2fe3500a73b5e6979fb3428
vansh-parikh <124344810+vansh-parikh@users.noreply.github.com>
2023-04-25 11:45:29 +0530

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import { UnivAbout } from "./pages/UnivAbout";
 import { Myprofile } from "./pages/Myprofile";
 import { EditMyProfile } from "./pages/EditMyProfile";
 import { UniversityDetails } from "./pages/Landing";
+import { ResetPassword } from "./pages/resetpassword";
+import { SendResetLink } from "./pages/SendResetLink";
 import { useNavigate } from "react-router-dom";
 import AdminHome from "./pages/AdminHome";
 import {
@@ -24,11 +26,67 @@ import { Link } from "react-router-dom";
 import FacultyDetails from "./pages/faculty";
 
 function App() {
+  var token=localStorage.getItem("token");
+  let navigate=useNavigate();
+  
+  const logOut = () => {
+    localStorage.setItem("token","null");
+    token=null;
+    alert("Logged out successfully");
+    navigate("/");
+  }
 
 
   return (
     <div className="App">
       {/* <Router> */}
+        <Box padding={1} border sx={{flexGrow: 1}}>
+          <AppBar position="static" className="Navbar">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              ></IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <b>Online Faculty-Staff Directory</b>
+              </Typography>
+              <Link to={"/"}>
+                <Button color="inherit" style={{ color: "white" }}>
+                  Home
+                </Button>
+              </Link>
+              <Button color="inherit" style={{ color: "white" }}>
+                Academics
+              </Button>
+              <Button color="inherit" style={{ color: "white" }}>
+                About Us
+              </Button>
+              {(token===null || token==="null")  && <Link to={"/loginpage"}>
+                <Button color="inherit" variant="outlined" sx={{ mr: 1, color: "white" }}>
+                  Sign In
+                </Button>
+              </Link>}
+              {(token===null || token==="null")  && <Link to={"/signup"}>
+                <Button color="warning" variant="contained">
+                  Sign Up
+                </Button>
+              </Link>}
+              {(token.length>4)  && <Link to={"/myprofile"}>
+                <Button color="inherit" variant="outlined" sx={{ mr: 1, color: "white" }}>
+                  My Profile
+                </Button>
+              </Link>}
+              {(token.length>4)   &&
+                <Button color="warning" variant="contained" onClick={logOut}>
+                  Log out
+                </Button>
+              }
+            </Toolbar>
+          </AppBar>
+        </Box>
         {/* {token.length>4 && token} */}
         <Routes>
           <Route path="/" element={<UniversityDetails />}></Route>
@@ -44,6 +102,9 @@ function App() {
           <Route path="/UniversityDetails" element={<UniversityDetails />} />
           <Route path="/Faculties" element={<FacultyDetails />} />
           <Route path="/adminhome" element={<AdminHome />} />
+          {/* <Route path="/resetpassword" element={<ResetPassword />} /> */}
+          <Route path="/resetpassword/:token" element={<ResetPassword />} />
+          <Route path="/sendresetlink" element={< SendResetLink />} />
         </Routes>
       {/* </Router> */}
     </div>
