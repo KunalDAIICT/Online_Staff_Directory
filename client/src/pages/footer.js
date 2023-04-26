d79c87e1c136b998df4ad26bbe64c41371664591
xxS1xx <123160190+xxS1xx@users.noreply.github.com>
2023-04-26 23:01:31 +0530

    Updated Footer

--- client/src/pages/footer.js
+++ client/src/pages/footer.js
@@ -1,74 +1,76 @@
-import React from "react";
-import "../footer.css";
+import {
+  Grid,
+  Box,
+  Typography,
+  Stack,
+} from '@mui/material'
 import da_logo from "../data/Logo.png"
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+
+
 
 export default function Footer() {
   return (
-    <footer className="footer-distributed">
-      <div className="footer-left">
-        <h3>
-          <span className="header-logo">
-            <img src={da_logo} alt="pic" height={100}/>
-          </span>
-        </h3>
-
-        <p className="footer-links">
-          <a href="/" className="link-1">
-            Home
-          </a>
-
-          <a href="/AboutUs">About</a>
-
-          <a href="/Login">Login</a>
-
-          <a href="/ContactUs">Contact</a>
-        </p>
-
-        <p className="footer-company-name">DAIICT Corp © 2023</p>
-      </div>
-
-      <div className="footer-center">
-        <div>
-          <p>
-            <span>DAIICT-campus, Reliance Cross Rd</span>Gandhinagar, Gujarat
-            382007
-          </p>
-        </div>
-
-        <div>
-          <br />
-          <p>+91.5555555555</p>
-        </div>
-
-        <div>
-          <p>
-            <a href="mailto:abc@gmail.com">abc@gmail.com</a>
-          </p>
-        </div>
-      </div>
-
-      <div className="footer-right">
-        <p className="footer-company-about">
-          <span>About the company</span>
-          Our website provides information of professional acheivement and contact of
-          faculties of multiple universities.
-        </p>
-
-        <div className="footer-icons">
-          <a href="facebook.com" className="footer-logo">
-            <img src="https://i.ibb.co/ZxC8NCp/Facebook.png" alt="pic" />
-          </a>
-          <a href="instagram.com" className="footer-logo">
-            <img src="https://i.ibb.co/4VXznmP/Instagram.png" alt="pic" />
-          </a>
-          <a href="#linkedin" className="footer-logo">
-            <img src="https://i.ibb.co/tzsnHYJ/LinkedIn.png" alt="pic" />
-          </a>
-          <a href="#twitter" className="footer-logo">
-            <img src="https://i.ibb.co/nb69Q63/Twitter.png" alt="pic" />
-          </a>
-        </div>
-      </div>
-    </footer>
+    <Box sx={{bgcolor: 'lightblue'}}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={3} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', mb: 7, mt: 2}}>
+            <img src={da_logo} alt="da_logo" width="35%"></img>
+        </Grid>
+        <Grid item xs={12} md={3} sx={{display: 'flex', justifyContent: 'center',  mb: 6, mt: 6}}>
+          <Stack direction="column">
+            <Typography >
+              Daiict college, Reliance Cross Rd, Gandhinagar, Gujarat 382007
+            </Typography>
+            <Typography  sx={{mt: 2}}>
+              +91 7968261700
+            </Typography>
+            <Typography sx={{'&:hover': {cursor: 'pointer', color: 'blue'}}}>
+              {/* add mail to mailto:faculty@id.com */}
+              campus_connect@gmail.com
+            </Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12} md={3} sx={{display: 'flex', justifyContent: 'center', mb: 7, mt: 4}}>
+            <Stack>
+              <Typography sx={{mb: 2}}>
+                <b>Useful Links</b>
+              </Typography>
+              <Typography align='center' sx={{'&:hover': {cursor: 'pointer', color: 'white'}}}>
+                Home
+              </Typography>
+              <Typography align='center' sx={{'&:hover': {cursor: 'pointer', color: 'white'}}}>
+                About
+              </Typography>
+              <Typography align='center' sx={{'&:hover': {cursor: 'pointer', color: 'white'}}}>
+                Login
+              </Typography>
+              <Typography align='center' sx={{'&:hover': {cursor: 'pointer', color: 'white'}}}>
+                Contact
+              </Typography>
+            </Stack>
+        </Grid>
+        <Grid item xs={12} md={3} sx={{display: 'flex', justifyContent: 'center',  mb: 7, mt: 4}}>
+          <Stack>
+            <Typography sx={{mb: 2}}>
+              <b>About the Company</b>
+            </Typography>
+            <Typography>
+              Our website provides information of professional acheivement and contact of faculties of multiple universities.
+            </Typography>
+            <Stack direction="row" spacing={2} sx={{mt: 2}}>
+              {/* Add links here */}
+              <FacebookIcon sx={{'&:hover': {cursor: 'pointer', color: 'white'}}}/> 
+              <InstagramIcon sx={{'&:hover': {cursor: 'pointer', color: 'white'}}}/>
+              <LinkedInIcon sx={{'&:hover': {cursor: 'pointer', color: 'white'}}}/>
+              <TwitterIcon sx={{'&:hover': {cursor: 'pointer', color: 'white'}}}/>
+            </Stack>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
