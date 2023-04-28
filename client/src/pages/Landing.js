e79302025128293c7094e43fd03560f88fb7fd09
xxS1xx <123160190+xxS1xx@users.noreply.github.com>
2023-04-28 12:55:42 +0530

    Updated Fonts in Landing Page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -16,21 +16,21 @@ export default function FrontPage() {
         <img src={front_page_img} alt="image not found" width='70%' align="center"></img>
       </Box>
       {/* <Divider width="100%"/> */}
-      <Container component="main" sx={{mt: 3, textAlign: "center", bgcolor: "lightcyan", borderRadius: 10, boxShadow: 3}}>
+      <Container component="main" sx={{mt: 10, textAlign: "center", bgcolor: "lightcyan", borderRadius: 10, boxShadow: 3}}>
         <Typography variant='h3' align="center" sx={{ mb: 4, mt: 2, pt: 3}}>
           Campus Connect
         </Typography>
-        <Typography variant="body1" align="center" sx={{ mb: 4 }}>
+        <Typography variant="body1" align="center" sx={{ mb: 4, fontSize: 18 }}>
           Our online faculty directory for multi-university is designed to provide a comprehensive listing of faculty members across multiple institutions. Our goal is to make it easier for students, researchers, and faculty members to connect and collaborate across universities, by providing a centralized hub for information on faculty members from multiple institutions.
         </Typography>
-        <Typography variant="body1" align="center" sx={{ mb: 4 }}>
+        <Typography variant="body1" align="center" sx={{ mb: 4, fontSize: 18 }}>
           Our team is dedicated to ensuring that our directory remains up-to-date and accurate, with regular updates to the information we provide. We understand the importance of having accurate information on faculty members, and strive to provide the most current information available.
         </Typography>
-        <Typography variant="body1" align="center" sx={{ mb: 4, pb: 3 }}>
+        <Typography variant="body1" align="center" sx={{ mb: 4, fontSize: 18, pb: 3 }}>
           Thank you for choosing our online faculty directory for multi-university. We hope you find our website useful and informative, and welcome any feedback or suggestions you may have.
         </Typography>
       </Container>
-      <Box sx={{display: 'flex', mt: 5, mb: 3, justifyContent: 'center'}}>
+      <Box sx={{display: 'flex', mt: 5, mb: 10, justifyContent: 'center'}}>
       </Box>
     </div>
   );
